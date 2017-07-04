907c06ad2075ae6c190b5214e82eec77488f12d7
Kevin Laurier <Kevin.Laurier@ie.ibm.com>
2017-07-04 17:05:48 +0000

    Correct service name

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -52,7 +52,7 @@ var settings = module.exports = {
     httpStatic: path.join(__dirname,"public"),
 
     functionGlobalContext: {
-    	VCAP_SERVICES: JSON.parse(process.env.VCAP_SERVICES)
+    	VCAP_SERVICES: VCAP_SERVICES
     },
 
     storageModule: require("./couchstorage")
